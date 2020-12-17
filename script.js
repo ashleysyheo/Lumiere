f0e9c09163e1e46c37e54bcc38969d44b8baf6dd
ashleysheo <ashleysyheo@gmail.com>
2020-12-17 17:54:04 +0900

    added loading screen

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ piano.appendChild(whiteKeys);
 
 const whiteCompKey = ['Z', 'X', 'C', 'V', 'B', 'N', 'M', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'];
 
-for (let i=0; i < 17; i++) {
+for (let i = 0; i < 17; i++) {
     const key = document.createElement('div');
     key.classList.add('white-key', whiteCompKey[i].toLowerCase());
     whiteKeys.appendChild(key);
@@ -21,7 +21,7 @@ piano.appendChild(blackKeys);
 
 const blackCompKey = ['S', 'D', 'G', 'H', 'J', '2', '3', '5', '6', '7', '9', '0'];
 
-for (let i=0; i<12; i++) {
+for (let i = 0; i < 12; i++) {
     const key = document.createElement('div');
     key.classList.add('black-key', `black-key${i+1}`);
     if (i < 5) {
@@ -36,3 +36,37 @@ for (let i=0; i<12; i++) {
     blackKeys.appendChild(key);
 }
 
+let sound = document.createElement('audio')
+sound.id = 'audio'
+sound.controls = 'controls'
+sound.src = './lumiere.mp3'
+sound.type = 'audio/mp3'
+document.body.appendChild(sound)
+
+function playAudio() {
+    document.getElementById('audio').play();
+}
+
+setTimeout("playAudio()", 6800);
+
+setTimeout(() => {
+    document.body.removeChild(sound);
+}, 10000)
+
+
+download_img = function (el) {
+    var image = canvas.toDataURL("image/jpg");
+    el.href = image;
+};
+
+
+window.addEventListener('keydown', function (e) {
+    if (e.key === 'Enter') {
+        var canvas = document.querySelector("canvas");
+        var image = canvas.toDataURL();
+        var downloadLink = document.createElement('a');
+        downloadLink.download = 'canvas_image.png';
+        downloadLink.href = image;
+        downloadLink.click();
+    }
+});
